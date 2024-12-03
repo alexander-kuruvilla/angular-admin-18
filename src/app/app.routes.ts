3e9653c43e0334ffe347ab4cc4f48e9af2838d23
Alexander <alexanderkalarickal@gmail.com>
2024-12-03 23:15:13 +0530

    added a table component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { DashboardHomeComponent } from './admin/dashboard-home/dashboard-home.component';
+import { TableComponent } from './admin/table/table.component';
 
 export const routes: Routes = [
     { path: 'home', component: DashboardHomeComponent},  
+    { path: 'table', component: TableComponent},  
     { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
