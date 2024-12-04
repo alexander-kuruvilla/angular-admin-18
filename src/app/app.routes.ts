6ab3f97cb948bb0ac863d36e75d29513138088b1
Alexander <alexanderkalarickal@gmail.com>
2024-12-04 23:15:36 +0530

    added routes and side bar entry for form component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardHomeComponent } from './admin/dashboard-home/dashboard-home.component';
 import { TableComponent } from './admin/table/table.component';
+import { FormComponent } from './admin/form/form.component';
 
 export const routes: Routes = [
     { path: 'home', component: DashboardHomeComponent},  
     { path: 'table', component: TableComponent},  
+    { path: 'form', component: FormComponent},  
     { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
