984ea756c7b1d6e376fe7edf74c456c21d80e93d
Alexander <alexanderkalarickal@gmail.com>
2024-12-07 09:01:30 +0530

    added chips

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardHomeComponent } from './admin/dashboard-home/dashboard-home.component';
 import { TableComponent } from './admin/table/table.component';
 import { FormComponent } from './admin/form/form.component';
+import { ChipsComponent } from './admin/chips/chips.component';
 
 export const routes: Routes = [
     { path: 'home', component: DashboardHomeComponent},  
     { path: 'table', component: TableComponent},  
     { path: 'form', component: FormComponent},  
+    { path: 'chips', component: ChipsComponent},  
     { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
